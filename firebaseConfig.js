56866f52bb90abce369bec49d7ac72566ec87c20
diazkimberly921-creator <your_email@example.com>
2025-08-27 10:28:31 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDk6gjFUUFoj4cDpANXAyhbEd4nk9EZa4Y",
+  authDomain: "kd-goals.firebaseapp.com",
+  projectId: "kd-goals",
+  storageBucket: "kd-goals.firebasestorage.app",
+  messagingSenderId: "1030762672663",
+  appId: "1:1030762672663:web:9efbec3213d7bec823a252"
+};
+//
+
+const app = initializeApp(firebaseConfig);
